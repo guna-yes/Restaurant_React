b043bc49e2c1ce825768bccd7b8421b68db31e39
Your Name <sgunasekharguna@gmail.com>
2020-04-22 17:07:22 +0530

    Uncontrolled forms login page

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron,Modal,ModalHeader,ModalBody,Button ,Form,FormGroup,Label,Input} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -8,9 +8,12 @@ class Header extends Component {
 
   
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen:false
         };
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal=this.toggleModal.bind(this);
+           this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav() {
@@ -18,11 +21,25 @@ class Header extends Component {
             isNavOpen: !this.state.isNavOpen
         });
     }
+    toggleModal(){
+        this.setState({
+            isModalOpen:!this.state.isModalOpen
+        })
+    };
+     handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
 
     render() {
         return (
             <div>
                 <Navbar dark expand="md">
+                
+
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />Restaurant</NavbarBrand>
@@ -43,6 +60,11 @@ class Header extends Component {
                             </Nav>
                         </Collapse>
                     </div>
+                     <Nav className="mr-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal} className="bg-light"><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
                 </Navbar>
                 <Jumbotron>
                     <div className="container">
@@ -54,6 +76,32 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                     <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
